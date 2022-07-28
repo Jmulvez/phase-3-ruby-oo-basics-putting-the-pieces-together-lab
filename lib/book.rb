ae1f810c0f6ba7a2b5ae1d2e0a8546918138e0a1
John <johnmulvey48@yahoo.com>
2022-07-28 11:13:38 -0400

    commit tree 6f487d370f6949aeddc55e247a36ee4a96c4c54f

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title)
+        @title = title,
+        @author = author,
+        @page_count = page_count,
+        @genre = genre
+    end
+end
 
-
+book1 = Book.new("And Then There Were None")
+book1.author = "me"
+book1.page_count = 27
+book1.genre = "comedy"
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
-# Make your shoe class here!
+class Shoe
+    attr_writer :brand, :color, :size, :material, :condition
+    def initialize(brand, color, size, material, condition)
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+end
+
+shoe1 = Shoe.new("Nike", "Red", 11, "Leather", "Good")
